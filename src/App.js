e23daff2b03cf3a084bd34edac0fd0b69ec724d1
SheikhAhmed135 <shiekhahmed135@gmail.com>
2021-11-14 23:34:55 +0500

    register page structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Route } from "react-router";
 import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
-  return <Route exact path="/register" component={Register} />;
+  return (
+    <>
+      <Route exact path="/register" component={Register} />,
+      <Route exact path="/login" component={Login} />
+    </>
+  );
 }
 
 export default App;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,50 @@
+import TextField from "@mui/material/TextField";
+
 import "./Register.scss";
+import useWindowDimensions from "./Dimension";
+import Background from "../assets/background.jpg";
 
 const Register = () => {
-  return <div className="register-page">Registration</div>;
+  const { height, width } = useWindowDimensions();
+
+  return (
+    <div className="register-page">
+      <div className="background">
+        <img src={Background} alt="Background" width="60%" height={height} />
+      </div>
+      <div className="register-form">
+        <form>
+          <div className="form-group">
+            <TextField required id="outlined-required" label="First Name" />
+          </div>
+          <div className="form-group">
+            <TextField required id="outlined-required" label="Last Name" />
+          </div>
+          <div className="form-group">
+            <TextField required id="outlined-required" label="Email" />
+          </div>
+          <div className="form-group">
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+          </div>
+          <div className="form-group">
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Confirm Password"
+              type="password"
+              autoComplete="current-password"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
