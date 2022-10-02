28d4610c30bcbc806311c87132b8ed150db9bdcb
Eber Alejo <eber4202@hotmail.com>
2022-10-02 18:26:48 -0500

    [FIX] routes no working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Route } from "react-router";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+
+const Home = () => <h1>Home page</h1>
+const Error404 = () => <h1>Error 404. Not found</h1>
 
 function App() {
   return (
     <>
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
