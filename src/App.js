0bf6731297ade2aa3c94d4143a32507d822ca606
Sushant Kumar <sushantkumar@Sushants-MacBook-Air.local>
2022-10-03 23:43:31 +0530

    Added the 404 component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route } from "react-router";
 import Register from "./components/Register";
 import Login from "./components/Login";
-
+import Error404 from "./components/Error404";
 function App() {
   return (
     <>
       <Route exact path="/register" component={Register} />
       <Route exact path="/login" component={Login} />
+      <Route path="*" component={Error404}/>
     </>
   );
 }
