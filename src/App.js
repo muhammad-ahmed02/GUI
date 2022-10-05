c60de700735fbf44efa5af5ad88bb5dc3edc3a90
devfaoed <faithadedokun2018@gmail.com>
2022-10-05 10:57:04 +0100

    GUI Blog

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route } from "react-router";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Error404 from "./components/Error404";
+import Blog from "./pages/blog/blog";
 function App() {
   return (
     <>
       <Route exact path="/register" component={Register} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/blog" component={Blog} />
       <Route path="*" component={Error404}/>
     </>
   );
