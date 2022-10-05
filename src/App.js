be9f629d189be979b149cb0f7f7284eb979bcd0c
Ahmed <shiekhahmed135@gmail.com>
2022-10-05 17:29:51 +0500

    blog page update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import Register from './components/Register'
-import Login from './components/Login'
-import Error404 from './components/Error404'
-import Home from './pages/Home'
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Error404 from "./components/Error404";
+import Home from "./pages/Home";
 
-function App () {
+import Blog from "./pages/blog/blog";
+function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-          <Route path='*' component={Error404} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/blog" component={Blog} />
+          <Route path="*" component={Error404} />
         </Switch>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
